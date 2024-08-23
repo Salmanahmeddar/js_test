ede53bb935afe353b50a747f81cd99ee5d2f9cd6
Salmanahmeddar <salmanahmeddar26@gmail.com>
2024-08-23 21:52:43 +0000

    Day 2

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1213131
+let accountEmail = "salman@gmail.com"
+var accountPassword = "12234"
+accountCity = "lahore"
+
+
+// accountId = 2
+accountEmail = "akdhfk"
+accountPassword = "akdckad"
+accountCity = "akdchan"
+console.table([accountId, accountEmail, accountPassword, accountCity])
